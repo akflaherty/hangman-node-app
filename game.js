b649e155c8c2d59eb03fbe62d2a4f8d1cff83960
Annie Flaherty <anne.flaherty@case.edu>
2018-01-17 15:13:27 -0500

    Added comments

--- game.js
+++ game.js
@@ -2,18 +2,17 @@ var Letter = require('./letter.js');
 var Word = require('./word.js');
 var inquirer = require('inquirer');
 
-var wordObj = new Word();
+var wordObj;
 
 function newGame() {
 	console.log('');	// new line for better formatting
 	console.log('New game started. Guess the word.');
     wordObj = new Word();
     wordObj.pickWord(); // pick a new word
-    // console.log(wordObj.word); // !!! FOR DEBUGGING
     wordObj.buildLetters(); // build array of letter objects
     console.log('Guesses remaining: ' + wordObj.trysLeft); // display guesses left
     wordObj.dispLetters(); // display letters or blanks
-    promptLetter();
+    promptLetter();	// ask user to guess a letter
 }
 
 function promptLetter() {
@@ -22,18 +21,18 @@ function promptLetter() {
         message: 'Guess a letter: ',
         name: 'letterGuess'
     }).then(function(inquirerResponse) {
+    	// check if the letter appears in the word
         wordObj.checkLetters(inquirerResponse.letterGuess);
-        // wordObj.trysLeft = wordObj.trysLeft - 1;
+        wordObj.trysLeft = wordObj.trysLeft - 1;	// decrement trys
         console.log('');
         console.log('Guesses remaining: ' + wordObj.trysLeft);
-        wordObj.dispLetters();
-        wordObj.trysLeft = wordObj.trysLeft - 1;
+        wordObj.dispLetters();	// update word display
         if (wordObj.lettersLeft == 0) {
-            console.log('WINNER');
+            console.log('You won! :)');
             promptNewGame();
             return;
         } else if (wordObj.trysLeft <= 0) {
-            console.log('LOSER');
+            console.log('You lost. :(');
             promptNewGame();
             return;
         } else {
@@ -43,6 +42,7 @@ function promptLetter() {
 }
 
 function promptNewGame() {
+	// ask user to confirm if they want a new game
 	inquirer.prompt({
 		type: 'confirm',
 		message: 'Start a new game?',
@@ -50,10 +50,11 @@ function promptNewGame() {
 	}).then(function(response) {
 		// console.log(response.inputContinue);
 		if (response.inputContinue) {
+			// if yes, start a new game
 			newGame();
 		}
+		// else quit
 	});
 }
 
-// newGame();
 promptNewGame();
